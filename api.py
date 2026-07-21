6e7e6dc348b9dd9eb5dccde3b9baee4e54d9b0dc
Munjal Patel <patelmunjal45@gmail.com>
2026-07-22 01:46:26 +0530

    Add production Vercel domain to CORS allowed origins

--- api.py
+++ api.py
@@ -92,6 +92,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:3000",
+        "https://finops-gold.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
